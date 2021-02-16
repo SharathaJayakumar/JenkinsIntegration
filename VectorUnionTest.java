cd7f2c68596e792d63b5f4c0fa702f74af14a695
SharathaJayakumar <77758920+SharathaJayakumar@users.noreply.github.com>
2021-02-16 14:37:23 -0600

    Update VectorUnionTest.java

--- VectorUnionTest.java
+++ VectorUnionTest.java
@@ -6,6 +6,9 @@ import junit.framework.Assert;
 import org.junit.Test; // for @Test
 import org.junit.Before; // for @Before
 
+import java.io.*; 
+import java.util.*; 
+
 
 
 public class VectorUnionTest {
@@ -18,8 +21,19 @@ public class VectorUnionTest {
 	}
 	
 	@Test
-	public void testEmptyQueueTest() {
+	public void testNullQueueTest() {
 		Assert.assertEquals(null, MyVector.union(null,null));
 	}
+
+	@Test
+	public void testEmptyQueueTest() {
+	    Vector<Integer> e1 = new Vector<Integer>(5); 
+        Vector<Integer> e2 = new Vector<Integer>(5); 
+		
+		Vector<Integer> r3 = MyVector.union(e1,e2);
+
+		Assert.assertTrue(r3.isEmpty());
+	}
+
 
 }
